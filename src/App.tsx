8d2429b55e2d9a9e65da2707b46c37c0674f02e6
Nicholas Charles Emmerson <nickemmerson619@hotmail.com>
2021-08-21 20:13:20 +0100

    Setting up the React frontend

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App: React.FC = () => (
+<p>This is the base App component.</p>
+);
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[tj]sx?$/,
+                use: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [ new HtmlWebpackPlugin({
+        template: './src/index.html',
+    })],
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx'],
+    },
+};
